e1abfac900734c4db45d46394a5cf3f6a21921a1
nanditaaaaaa <134855694+nanditaaaaaa@users.noreply.github.com>
2023-06-15 23:16:29 +0700

    Update auth-validation.js

--- Cloud Computing/api-foodmoodfinder/middleware/auth-validation.js
+++ Cloud Computing/api-foodmoodfinder/middleware/auth-validation.js
@@ -1,13 +1,13 @@
 // import check dari express validator
 const { check } = require("express-validator");
 
-// mengecek validasi register 
+//cek validasi register 
 exports.registerValidation = [
-    check("email", "Isi dengan email yang valid.").isEmail().normalizeEmail({ gmail_remove_dots: true }),
-    check("pass", "Password harus diisi minimal 6 kata").isLength({ min: 6 }),
+    check("email", "Harus diisi dengan email").isEmail().normalizeEmail({ gmail_remove_dots: true }),
+    check("pass", "Password minimal 6 kata").isLength({ min: 6 }),
 ];
 
-// mengecek validasi login
-exports.loginValidation = [check("email", "Isi dengan email yang valid").isEmail().normalizeEmail({ gmail_remove_dots: true }), 
-check("Password harus diisi minimal 6 kata").isLength({ min: 6 })];
+// cek validasi login
+exports.loginValidation = [check("email", "Harus diisi dengan email").isEmail().normalizeEmail({ gmail_remove_dots: true }), 
+check("Password minimal 6 kata").isLength({ min: 6 })];
 
